787cbe3dd25cb04948ede6e3a1235c5fbc1df94c
saim hashmi <hashmisaim037@gmail.com>
2024-10-27 15:56:03 -0400

    update api.ts

--- service/src/config/api.ts
+++ service/src/config/api.ts
@@ -12,7 +12,6 @@ class Application {
 
   constructor() {
     this.server = express();
-    this.server.set("port", process.env.HOST || "0.0.0.0"); // Changed to 0.0.0.0
     this.server.set("port", process.env.PORT || 5000);
     this.server.use(morgan("dev"));
     this.server.use(helmet());
@@ -24,11 +23,9 @@ class Application {
   }
 
   public start(): void {
-    const host: string = this.server.get("host");
     const port: number = this.server.get("port");
-
-    this.server.listen(port, host, () => {
-      console.log(`Server started at http://${host}:${port}`);
+    this.server.listen(port, () => {
+      console.log(`Server started on port ${port}`);
     });
   }
 }
