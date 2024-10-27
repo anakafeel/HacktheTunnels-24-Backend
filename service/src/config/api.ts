94dd7b788d1336416179888282b0a7126871086d
saim hashmi <hashmisaim037@gmail.com>
2024-10-27 16:26:38 -0400

    removing cors

--- service/src/config/api.ts
+++ service/src/config/api.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
 import dotenv from "dotenv";
 import { router, notFound, errorHandler } from "../api";
 
@@ -15,15 +15,7 @@ class Application {
     this.server.set("port", process.env.PORT || 5000);
     this.server.use(morgan("dev"));
     this.server.use(helmet());
-
-    // Configure CORS to allow your frontend
-    const corsOptions: CorsOptions = {
-      origin: "https://hackthe-tunnels-24.vercel.app/login", 
-      methods: ["GET", "POST", "PUT", "DELETE"], 
-      credentials: true, 
-    };
-
-    this.server.use(cors(corsOptions));
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use("/api/v1", router);
     this.server.use(notFound);
