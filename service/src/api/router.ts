d31cdb8ed1edf73548d830568ea50b43d2b1fcba
saim hashmi <hashmisaim037@gmail.com>
2024-10-29 15:59:45 -0400

    working dev - 5

--- service/src/api/router.ts
+++ service/src/api/router.ts
@@ -3,18 +3,15 @@ import { AccountRouter, ScheduledEventRouter, TimetableRouter } from "./routes";
 
 const router = express.Router();
 
-// Define your API routers first
-router.use("/account", AccountRouter); // Assuming AccountRouter has its own paths
-router.use("/scheduledEvents", ScheduledEventRouter);
-router.use("/timetables", TimetableRouter);
-
-// Then define the root route
 const getAPIRoot = async (_: Request, response: Response) => {
   response.json({
     message: "API - 👋",
   });
 };
 
-router.get("/", getAPIRoot); // This should now work correctly
+router.get("/", getAPIRoot);
+router.use("/", AccountRouter);
+router.use("/scheduledEvents", ScheduledEventRouter);
+router.use("/timetables", TimetableRouter);
 
 export default router;
